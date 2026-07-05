a4a613f94c38cb42846361385b33a4dd4fe4b360
Krishna Gupta <krishgpt1807@gmail.com>
2026-07-05 22:18:54 +0530

    Update PostEdit.jsx

--- src/pages/PostEdit.jsx
+++ src/pages/PostEdit.jsx
@@ -1,5 +1,5 @@
 import PostForm from "../components/PostForm"
-import useLocalStorage from "../hooks/useLocalStorage"
+import useLocalStorage from "../hooks/UseLocalStorage"
 import { useNavigate, useParams } from "react-router-dom"
 
 function PostEdit () {
